f7686a186777bda79d5e33e3ad0589eb7c00643a
DrInnovatio <turing1723@gmail.com>
2020-08-25 15:47:42 +1000

    added CardItem.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,6 +7,7 @@ function HeroSection () {
   return (
     <div className='hero-container'>
       <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <h1>Making New Connections</h1>
       <p>What are you doing for?</p>
       <div className='hero-btns'>
         <Button
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,9 +4,9 @@ import HeroSection from '../HeroSection.jsx'
 
 function Home () {
   return (
-    <div>
+    <>
       <HeroSection />
-    </div>
+    </>
   )
 }
 
